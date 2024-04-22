ae93b88314fe9ca99caa273e24ece7b214c96fea
Alejandro Durango <ddurango712@soyudemedellin.edu.co>
2024-04-22 08:49:32 -0500

    finalización offset

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# scroll_infinite
 
-A new Flutter project.
+# Proyecto de Listado de Pokemons con Scroll Infinito
+
+Este proyecto es una aplicación de Flutter que muestra una lista de Pokemons con scroll infinito. Los usuarios pueden desplazarse por la lista para ver los diferentes Pokemons disponibles y la aplicación cargará más Pokemons a medida que el usuario se desplace hacia abajo.
+
+## Características
+
+- **Lista de Pokemons**: La aplicación muestra una lista de Pokemons utilizando un `ListView.builder`. Cada elemento de la lista es un `ListTile` que muestra el nombre del Pokemon.
+
+- **Scroll Infinito**: La aplicación utiliza un `ScrollController` para detectar cuando el usuario se ha desplazado hasta el final de la lista. Cuando esto sucede, la aplicación carga más Pokemons.
+
+## Código Principal
+
+El código principal de la aplicación se encuentra en los siguientes archivos:
+
+- `main.dart`: Este archivo contiene la función `main` que inicia la aplicación y el widget `MainApp` que define la estructura principal de la aplicación.
+
+- `ListPokemons.dart`: Este archivo contiene el widget `ListPokemons` que muestra la lista de Pokemons y gestiona el scroll infinito.
+
+## Cómo Ejecutar
+
+Para ejecutar esta aplicación, necesitarás tener Flutter y Dart instalados en tu máquina. Una vez instalados, puedes clonar este repositorio y ejecutar `flutter run` en la raíz del proyecto.
+
+## Contribuir
+
+Las contribuciones son bienvenidas. Por favor, abre un issue o un pull request si deseas contribuir a este proyecto.
+
+## Licencia
+
+Este proyecto está licenciado bajo los términos de la licencia MIT.
--- lib/views/ListPokemons.dart
+++ lib/views/ListPokemons.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:scroll_infinite/controllers/PokemonController.dart';
@@ -15,15 +17,27 @@ class _ListPokemonsState extends State<ListPokemons> {
   int _offset = 0;
   bool _loading = false;
   List<Pokemon> _pokemons = [];
+  final ScrollController _scroll = ScrollController();
 
   @override
   void initState() {
-    // TODO: implement initState
-    
-   getPokemons();
     super.initState();
+    getPokemons();
+    _scroll.addListener(() {
+      double max = _scroll.position.maxScrollExtent;
+      double currentPosition = _scroll.position.pixels;
+      if ((currentPosition + 300 >= max) && !_loading) {
+        getPokemons();
+      }
+    });
   }
-void getPokemons() async {
+
+  createAnimate(double offset) {
+    _scroll.animateTo(offset,
+        duration: const Duration(microseconds: 300), curve: Curves.bounceIn);
+  }
+
+  void getPokemons() async {
     setState(() {
       _loading = true;
     });
@@ -33,16 +47,43 @@ void getPokemons() async {
       _offset += 20;
       _loading = false;
     });
-  }
 
+    double currentPosition = _scroll.position.pixels;
+    double max = _scroll.position.maxScrollExtent;
+    double offset = currentPosition + 100;
+    if (offset >= max) createAnimate(offset);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return createListView();
+    return RefreshIndicator(
+      onRefresh: () async {
+        // implement any logic here
+        _pokemons = await _controller.getPokemons(offset: 0);
+        setState(() {
+          _offset = 0;
+        });
+      },
+      child: Stack(
+        children: [
+          createListView(),
+          if (_loading) createProgress(),
+        ],
+      ),
+    );
   }
-  
+
+  createProgress() {
+    return Positioned(
+      bottom: 10,
+      left: MediaQuery.of(context).size.width / 2 - 10,
+      child: CircularProgressIndicator(),
+    );
+  }
+
   ListView createListView() {
     return ListView.builder(
+      controller: _scroll,
       itemBuilder: (context, index) {
         Pokemon pokemon = _pokemons[index];
         return ListTile(
